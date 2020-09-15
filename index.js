5c968e2db6a9f928e780659737739f2f84ca0afd
adesurya <adesurya.tkj@gmail.com>
2020-09-15 11:39:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Ade!')
+  res.send('Hello Ade Surya!')
 })
 //Launch listening server on port 8080 and consoles the log.
 app.listen(8080, function () {
